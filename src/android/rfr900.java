8b169a1ddf41af733bf2116aff6dc7113e6da3d4
Svein Gunnar Larsen <Svein Gunnar Larsen>
2017-08-11 12:44:11 +0200

    update

--- src/android/rfr900.java
+++ src/android/rfr900.java
@@ -17,6 +17,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.os.Handler;
 import co.kr.bluebird.ser.protocol.Reader;
 
 
